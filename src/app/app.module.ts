04041b9d25fb4bd0bb83465d5b2065b33d68e791
kelvinhovinho <kelvinadamba@hotmail.com>
2021-02-02 01:25:51 +0300

    add form.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteComponent } from './quote/quote.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteDetailsComponent,
-    QuoteComponent
+    QuoteComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
+import { Quote } from '../quote';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  newQuote = new Quote(0,'', '', '', 0, 0, new Date());
+  @Output () addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+    this.newQuote =  new Quote(0,'', '', '', 0, 0, new Date());
+    
+    
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
